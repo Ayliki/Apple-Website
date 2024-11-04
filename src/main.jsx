790a20a5a0d9009df2a77ca6e5f2bee6f808ed23
Ilya Belov <ilyabelov518@gmail.com>
2024-11-04 17:48:37 +0100

    Initial commit - replicate Apple's website project

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7db7257d29ed0c72494c63ec25a23bd3@o4508240532013056.ingest.de.sentry.io/4508240534831184",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }), 
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
